e6f4d0d82ca43216fc3e9c778c72abb54ee9ad0f
jiyong <parkjiy96@naver.com>
2025-06-03 20:25:30 +0900

    auth및 main페이지 분리리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import styles from "@/styles/page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div className={styles.page}>gd</div>;
+  return (
+    <div className={styles.page}>
+      <Link href={"/main"}>main</Link>
+      <div>---------------</div>
+      <div>---------------</div>
+      <div>---------------</div>
+      <Link href={"/auth"}>auth</Link>
+    </div>
+  );
 }
